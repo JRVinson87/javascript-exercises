646ece897ef283252b4c90db0f41044cdd5d6df8
Joshua Vinson <JRVinson87@gmail.com>
2022-04-21 19:59:05 -0600

    solved tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp -= 32;
+  temp *= (5 / 9);
+  return Math.round(temp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp *= (9 / 5);
+  temp += 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
