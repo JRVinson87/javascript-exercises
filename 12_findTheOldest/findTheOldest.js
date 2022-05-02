b7d07a496851288d2451557c78f7166e84962381
Joshua Vinson <JRVinson87@gmail.com>
2022-05-01 23:49:45 -0600

    Solved findTheOldest exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    return obj.reduce((a, b) => {
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        } else if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        
+        const previous = (a.yearOfDeath - a.yearOfBirth);
+        const current = (b.yearOfDeath - b.yearOfBirth);
+        return previous > current ? a : b;
+    })
 };
 
 // Do not edit below this line
