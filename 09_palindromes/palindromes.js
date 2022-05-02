7a4535173852429417d35e61630537a724cd5daf
Joshua Vinson <JRVinson87@gmail.com>
2022-05-01 23:03:16 -0600

    Solved palindrome exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Clean out all non alpha characters.
+    let cleaned = /[\W]/gi;
+    let forward = str.toLowerCase().replace(cleaned, "");
+    let backward = forward.split('').reverse().join('');
+    // console.log(forward);
+    // console.log(backward);
+    return forward === backward;
 };
 
 // Do not edit below this line
