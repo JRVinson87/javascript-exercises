4716889efeb2cc923e6348987892269a569db978
Joshua Vinson <JRVinson87@gmail.com>
2022-05-01 21:49:30 -0600

    Solved calculator exercises.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length < 1) {
+    return 0;
+  } else {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length < 1) {
+    return 0;
+  } else {
+    let total = 1;
+    for (let i = 0; i < arr.length; i++) {
+      total *= arr[i];
+    }
+    return total;
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 2) {
+    return 1;
+  } else {
+    return factorial(num - 1) * num;
+  }
 };
 
 // Do not edit below this line
